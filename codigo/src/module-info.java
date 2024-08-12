3ed4b33e39ace00698c18416fefe33499c0c1936
0xSoul <31786518+0xSoul@users.noreply.github.com>
2024-08-12 08:34:34 -0500

    Add module-info file for JFX indexing

---
+++ codigo/src/module-info.java
@@ -0,0 +1,10 @@
+module ahorcado {
+	requires java.desktop;
+	requires java.logging;
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.graphics;
+
+	opens controlador;
+}
